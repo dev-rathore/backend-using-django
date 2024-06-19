13f805a402ed97cc377f7deb8d534ef05f56a9e4
dev-rathore <rathoredvd3001@gmail.com>
2024-06-20 02:13:16 +0530

    urls with params

--- backendUsingDjango/myfirstapp/urls.py
+++ backendUsingDjango/myfirstapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # localhost:8080/myfirstapp/
 urlpatterns = [
-  path('', views.my_first_app, name='my_first_app')
+  path('', views.my_first_app, name='my_first_app'),
+  path('<int:id>/', views.my_first_app_detail, name='my_first_app_detail'),
 ]
--- backendUsingDjango/myfirstapp/views.py
+++ backendUsingDjango/myfirstapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import MyFirstModel
 
 # Create your views here.
 def my_first_app(request):
-  return render(request, 'myfirstapp/index.html')
+  datas = MyFirstModel.objects.all()
+  return render(request, 'myfirstapp/index.html', {'datas': datas})
+
+def my_first_app_detail(request, id):
+  data = get_object_or_404(MyFirstModel, pk=id) # pk is the primary key of the data
+  return render(request, 'myfirstapp/detail.html', {'data': data})
