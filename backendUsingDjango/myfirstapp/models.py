5090402620daf6a7f2b6249f56433830adf61ccc
dev-rathore <rathoredvd3001@gmail.com>
2024-06-20 02:12:51 +0530

    models

--- backendUsingDjango/myfirstapp/models.py
+++ backendUsingDjango/myfirstapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class MyFirstModel(models.Model):
+  OCCUPATION_CHOICES = [
+    ('S', 'Student'),
+    ('T', 'Teacher'),
+    ('D', 'Developer'),
+    ('O', 'Other'),
+  ]
+  name = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='images/')
+  date_added = models.DateTimeField(default=timezone.now)
+  occupation = models.CharField(max_length=1, choices=OCCUPATION_CHOICES)
+  description = models.TextField(default='')
+
+  def __str__(self):
+    return self.name
