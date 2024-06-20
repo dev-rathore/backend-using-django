0aefaf8e20a858176d50ad2a97196e066c570d24
dev-rathore <rathoredvd3001@gmail.com>
2024-06-21 02:54:29 +0530

    Relationship between models

--- backendUsingDjango/myfirstapp/admin.py
+++ backendUsingDjango/myfirstapp/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from .models import MyFirstModel
+from .models import MyFirstModel, Review, Tag, Certificate
 
 # Register your models here.
-admin.site.register(MyFirstModel)
+admin.site.register(MyFirstModel) # Freelancer
+admin.site.register(Review)
+admin.site.register(Tag)
+admin.site.register(Certificate)
+
+# class ReviewInline(admin.TabularInline):
+  # model = Review
+  # extra = 2
+
+# class MyFirstModelAdmin(admin.ModelAdmin):
+  # list_display = ('name', 'date_added', 'occupation', 'description')
+  # inlines = [ReviewInline]
+
+# class TagInline(admin.ModelAdmin):
+  # list_display = ('name', 'location', 'freelancers')
+  # filter_horizontal = ('freelancers')
+
+# class CertificateAdmin(admin.ModelAdmin):
+  # list_display = ('freelancer', 'certificate_name', 'issued_date')
+  # filter_horizontal = ('freelancer')
+
+# admin.site.register(MyFirstModel, MyFirstModelAdmin)
+# admin.site.register(Tag, TagInline)
+# admin.site.register(Certificate, CertificateAdmin)
--- backendUsingDjango/myfirstapp/models.py
+++ backendUsingDjango/myfirstapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
-class MyFirstModel(models.Model):
+class MyFirstModel(models.Model): # Freelancer
   OCCUPATION_CHOICES = [
     ('S', 'Student'),
     ('T', 'Teacher'),
@@ -17,3 +18,32 @@ class MyFirstModel(models.Model):
 
   def __str__(self):
     return self.name
+
+# One to Many Relationship
+class Review(models.Model):
+  freelancer = models.ForeignKey(MyFirstModel, on_delete=models.CASCADE, related_name='reviews')
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  rating = models.IntegerField()
+  comment = models.TextField()
+  date_added = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return f'{self.freelancer.name} serving for {self.user.username}'
+
+# Many to Many Relationship
+class Tag(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  freelancers = models.ManyToManyField(MyFirstModel, related_name='tags')
+
+  def __str__(self):
+    return self.name
+
+# One to One Relationship
+class Certificate(models.Model):
+  freelancer = models.OneToOneField(MyFirstModel, on_delete=models.CASCADE, related_name='certificate')
+  certificate_name = models.CharField(max_length=100)
+  issued_date = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return f'{self.freelancer.name} is a certified freelancer'
